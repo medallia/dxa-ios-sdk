c8838001eb96bfd56dd24d1e597b644f799b2c7c
Jenkins <jenkins@Jenkinss-Mac-Pro-2.local>
2026-09-10 11:19:21 -0600

    Jenkins Release Job 10 message: chore(release): 3.0.0-alpha

--- Package.swift
+++ Package.swift
@@ -2,21 +2,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "dxa-ios-sdk",
+    name: "dxa-ios-flutter-sdk",
     platforms: [
         .iOS(.v15)
     ],
     products: [
-        .library(name: "dxa-ios-sdk", targets: ["MedalliaDXASDKWrapper"])
+        .library(name: "dxa-ios-flutter-sdk", targets: ["MedalliaDXAFlutterSDKWrapper"])
     ],
     dependencies: [
         .package(name: "MedalliaBridgeSDK", url: "https://github.com/medallia/mobile-ios-bridge-sdk.git", .upToNextMajor(from: "1.3.1"))
     ],
     targets: [
-        .binaryTarget(name: "MedalliaDXA", path: "MedalliaDXA.xcframework"),
-        .target(name: "MedalliaDXASDKWrapper", dependencies: [
-            .target(name: "MedalliaDXA"),
+        .binaryTarget(name: "MedalliaDXAFlutter", path: "MedalliaDXAFlutter.xcframework"),
+        .target(name: "MedalliaDXAFlutterSDKWrapper", dependencies: [
+            .target(name: "MedalliaDXAFlutter"),
             .product(name: "medallia-mobile-bridge-ios-sdk", package: "MedalliaBridgeSDK")
-        ], path: "Sources/MedalliaDXASDKWrapper")
+        ], path: "Sources/MedalliaDXAFlutterSDKWrapper")
     ]
 )
